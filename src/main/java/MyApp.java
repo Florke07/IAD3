d2fb5f6f3de4d98c9838b510f5c8eeb0635db01c
Florke07 <maciek.socha1@gmail.com>
2018-06-01 14:11:57 +0200

    Wczyt doubli z pliku

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,5 +1,17 @@
+import reader.ReadData;
+import reader.ReadFromTXTWindows;
+
+import java.util.ArrayList;
+
 public class MyApp {
     public static void main(String args[]) {
         System.out.println("Hello");
+
+        ReadData reader = new ReadFromTXTWindows();
+        ArrayList<Double> list = new ArrayList<>();
+        list = reader.readDouble("data/approximation_train_1.txt");
+        for (Double d: list) {
+            System.out.println(d);
+        }
     }
 }
--- src/main/java/reader/ReadFromTXTWindows.java
+++ src/main/java/reader/ReadFromTXTWindows.java
@@ -1,6 +1,9 @@
 package reader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReadFromTXTWindows implements ReadData {
     @Override
@@ -10,7 +13,20 @@ public class ReadFromTXTWindows implements ReadData {
 
     @Override
     public ArrayList<Double> readDouble(String pathToFile) {
-        return null;//todo implementacja odczytu win
+        ArrayList<Double> ret = new ArrayList<>();
+        File file = new File(pathToFile);
+        Scanner in;
+        String tmp="";
+        try {
+            in = new Scanner(file);
+            while (in.hasNextLine()) {
+                tmp = in.next();
+                ret.add(Double.parseDouble(tmp));
+            }
+        } catch (FileNotFoundException ex ) {
+            ex.printStackTrace();
+        }
+        return ret;
     }
 
     @Override
