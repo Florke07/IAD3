160ae2ab272b4b65293528df56abf082007334ff
Florke07 <maciek.socha1@gmail.com>
2018-06-01 14:17:34 +0200

    Wczyty są zrobione

--- src/main/java/reader/ReadData.java
+++ src/main/java/reader/ReadData.java
@@ -3,9 +3,7 @@ package reader;
 import java.util.ArrayList;
 
 public interface ReadData {
-    ArrayList<Integer> readInt(String pathToFile);
 
     ArrayList<Double> readDouble(final String pathToFile);
 
-    ArrayList<String> readStringLines(final String pathToFile);
 }
--- src/main/java/reader/ReadFromTXTWindows.java
+++ src/main/java/reader/ReadFromTXTWindows.java
@@ -6,31 +6,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadFromTXTWindows implements ReadData {
-    @Override
-    public ArrayList<Integer> readInt(String pathToFile) {
-        return null;//todo implementcja odczytu win
-    }
 
     @Override
     public ArrayList<Double> readDouble(String pathToFile) {
         ArrayList<Double> ret = new ArrayList<>();
         File file = new File(pathToFile);
         Scanner in;
-        String tmp="";
+        String tmp = "";
         try {
             in = new Scanner(file);
             while (in.hasNextLine()) {
                 tmp = in.next();
                 ret.add(Double.parseDouble(tmp));
             }
-        } catch (FileNotFoundException ex ) {
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
         return ret;
-    }
-
-    @Override
-    public ArrayList<String> readStringLines(String pathToFile) {
-        return null;//todo implementacja odczytu win
     }
 }
