848e911399f39611defdc902263d8b5be223e0cb
Florke07 <maciek.socha1@gmail.com>
2018-06-12 12:53:58 +0200

    aproksymacja poprawa

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -19,40 +19,40 @@ public class MyApp {
 
         double[][] funkcja = new double[1000][2];
         double[][] wynik = new double[1000][2];
-        int k=0;
-        for (int i =0;i<testData.size();i+=2) {
+        int k = 0;
+        for (int i = 0; i < testData.size(); i += 2) {
             funkcja[k][0] = testData.get(i);
-            funkcja[k][1] = testData.get(i+1);
+            funkcja[k][1] = testData.get(i + 1);
             k++;
         }
-       // DrawPlot.draw(funkcja);
-        int iloscEpokR=100;
+        // DrawPlot.draw(funkcja);
+        int iloscEpokR = 100;
         int iloscEpokMLP = 1000;
-        RBFNetwork RBF = new RBFNetwork(list,20,1,1,iloscEpokR);
+        RBFNetwork RBF = new RBFNetwork(list, 20, 1, 1, iloscEpokR);
         int g;
 
-        for(int e =0;e<iloscEpokMLP;e++){
+        for (int e = 0; e < iloscEpokMLP; e++) {
             for (int i = 0; i < list.size(); i += 2) {
 
                 in.clear();
                 exp.clear();
                 in.add(list.get(i));
                 exp.add(list.get(i + 1));
-                RBF.learnMLPLayer(in,exp);
+                RBF.learnMLPLayer(in, exp);
             }
         }
 
 
-        g=0;
-        for (int i=0;i<testData.size();i+=2) {
+        g = 0;
+        for (int i = 0; i < testData.size(); i += 2) {
             in.clear();
             in.add(testData.get(i));
             out = RBF.feedForward(in);
 
-            wynik[g][0]=in.get(0);
-            wynik[g][1]=out.get(0);
+            wynik[g][0] = in.get(0);
+            wynik[g][1] = out.get(0);
             g++;
         }
-        DrawPlot.draw(funkcja,wynik);
+        DrawPlot.draw(funkcja, wynik);
     }
 }
